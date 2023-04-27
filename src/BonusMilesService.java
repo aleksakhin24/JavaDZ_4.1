468cc0a182bfc1157fc2f9beb240be51b6ce397b
Gregory <zmey106@yandex.ru>
2023-04-27 20:21:47 +0300

    Java dz 1

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
-    public int calculate(int a) {
-        int bonus = a / 20;
+    public int calculate(int price) {
+        int bonus = price / 20;
         return bonus;
     }
 }
